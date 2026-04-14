6869f431c37cc0cf9159d31ccf93328fb12d342d
FR4NCISCO1106 <francisquito1120@gmail.com>
2026-04-14 17:20:59 -0400

    feat

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Dashboard - SB Adminz</title>
+        <link rel="icon" type="image/jpg" sizes="512x512" href="./img/icon2.png">
+        <title>Sistema de solicitudes</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
